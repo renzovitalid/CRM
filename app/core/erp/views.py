7659cefb43d647cd6089b57de2c95d4c3de4a84c
renzovitalid <renzo.vitali.delgado@gmail.com>
2020-11-25 20:05:13 -0300

    updated v1.5 clase 18

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -5,7 +5,7 @@ def myfirstview (request):
         'name':'William',
         'categories' : Category.objects.all()
     }
-    return render(request,'index.html', data)
+    return render(request,'home.html', data)
 
 def mysecondview (request):
     data = {
